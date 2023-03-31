7ffcc37b54a796988f62322adf7053a5bbae37f7
Darshan Bhadiyadra <129250327+Darshan-Bhadiyadra@users.noreply.github.com>
2023-03-31 10:17:13 +0530

    Add New NFT PORT Site

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    $('#slide1').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplaySpeed:2000,
+        slideBy:1,
+        dots:false,
+        responsive:
+            {
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:3
+                }
+            }
+        });
+
+        $('#slimg2').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplaySpeed:2000,
+        slideBy:1,
+        dots:false,
+        responsive:
+            {
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:3
+                }
+            }
+        });
+
+});
